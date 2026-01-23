dd326a6a00839b1f188ef0bbd480e0eb0d24f763
小宇 <58876608+luluxiaoyu@users.noreply.github.com>
2026-01-23 08:52:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -47,7 +47,9 @@ export default {
       
 
       if (inputSecret !== SECRET) {
-        return responseJSON(403, "接口鉴权失败: Secret 错误或丢失");
+        return responseJSON(403, "接口鉴权失败: Secret 错误或丢失", {
+             tip: Object.keys(env).join(',')
+        });
       }
 
       // --- 验证码结果检查 ---
